c3eb0e115b8a596314237b5def8f5857389c6d76
cynthiacarter1 <cynthia.carter@myemail.indwes.edu>
2015-09-16 18:05:42 +0000

    CarterWeekone.py

--- chaos.py
+++ chaos.py
@@ -1,13 +1,14 @@
 __author__ = 'daniel.neumann'
-
+# Edit by Cynthia Carter
+# CIS 125 Fall 2015
 # File: chaos.py
 # A simple program illustrating chaotic behavior.
 
 def main():
     print("This program illustrates a chaotic function")
     x = eval(input("Enter a number between 0 and 1: "))
-    for i in range(10):
-        x = 3.9 * x * (1 - x)
+    for i in range(20):
+        x = 2 * x * (1 - x)
         print(x)
 
 main()
